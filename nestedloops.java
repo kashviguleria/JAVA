8c214d545924aaa41754251eec4dfb9c74d8ada1
Kashvi Guleria <kashviguleria@gmail.com>
2026-07-03 10:51:01 +0530

    Add files via upload

---
+++ nestedloops.java
@@ -0,0 +1,15 @@
+
+
+public class nestedloops {
+    public static void main(String[] args) {
+        //if loop runs like i=5 j=5 so total will be 5*5=25
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(i+" "+j+"  ");
+            }
+            System.out.println();
+        }
+
+    }
+    }
+
